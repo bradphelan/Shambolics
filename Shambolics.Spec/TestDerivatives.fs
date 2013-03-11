78042fac5b2902499c9a18c679157c31dcc81ee9
Brad Phelan <bradphelan@xtargets.com>
2013-03-11 23:00:30 +0100

    Get all test passing
    Requires getting the simplification engine
    rules cleaned up and made more robust and
    extensive

--- Shambolics.Spec/TestDerivatives.fs
+++ Shambolics.Spec/TestDerivatives.fs
@@ -9,9 +9,26 @@ open FSharpx.Linq.QuotationEvaluation
 open System
 open Microsoft.FSharp.Linq.RuntimeHelpers
 
+type EqualsAsString (e:obj)= 
+    inherit NUnit.Framework.Constraints.Constraint()
+    
+    let expected = e
+    
+    override x.Matches(actual:obj)=
+        x.actual <- actual
+        x.actual.ToString() = expected.ToString()
+        
+    override x.WriteDescriptionTo(writer)=
+        writer.WriteExpectedValue(expected)
+        
+    override x.WriteActualValueTo(writer)=
+        writer.WriteActualValue(x.actual)
+   
 [<TestFixture>]
 type ``This is a test for symbolic derivatives`` ()=
 
+    let equalExpr (x:Expr<'t>) = new EqualsAsString(x)
+    
     [<Test>] 
     member x.``Derivative should work`` ()=
     
@@ -19,30 +36,54 @@ type ``This is a test for symbolic derivatives`` ()=
         let d = der e
         
         let l:double->double=LeafExpressionConverter.EvaluateQuotation d :?> ( double -> double )
-        
-        let x = <@ fun (y:double) -> 1.0 * y + y * 1.0 @>
-        d |> should equal x     
+        d |> should equalExpr <@ fun (y:double) -> 2.0 * y @>
              
         
     [<Test>] 
-    member x.``Trig should work`` ()=
+    member x.``Sin should differentiate`` ()=
         
         let e = <@ fun (y:double) -> Math.Sin( 2.0 * y * y ) @>
         let d = der e
         
-        println d
+        d |> should equalExpr <@ fun (y:double) -> 4.0 * y * Math.Cos(2.0 * y * y ) @>
         
-        d |> should equal <@ fun (y:double) -> 4.0 * y * Math.Cos(2.0 * y * y ) @>
+    [<Test>] 
+    member x.``Cos should differentiate`` ()=
         
+        let e = <@ fun (y:double) -> Math.Cos( 2.0 * y * y ) @>
+        let d = der e
+        
+        d |> should equalExpr <@ fun (y:double) -> - (4.0 * y) * Math.Sin(2.0 * y * y ) @>
 
     [<Test>] 
     member x.``Multipy by zero should work`` ()=
     
         let e = simplify <@ fun x -> 0.0 * x @>
-        e |> should equal <@ fun x-> 0.0 @>
+        e |> should equalExpr <@ fun x-> 0.0 @>
         
     [<Test>] 
     member x.``Add zero should work`` ()=
         
         let e = simplify <@ fun x -> 0.0 + x @>
-        e |> should equal <@ fun x-> x @>
+        e |> should equalExpr <@ fun x-> x @>
+        
+        
+    [<Test>] 
+    member x.``Simplify addition of similar terms should work`` ()=
+        let e = simplify <@ fun (x:double) -> (x+2.0) + (x+2.0) @>
+        e |> should equalExpr <@ fun x-> 2.0 * (x + 2.0) @>
+
+    [<Test>] 
+    member x.``Extract common factors`` ()=
+        let e = simplify <@ fun (x:double) -> (x * 2.0) + (x * 3.0) @>
+        e |> should equalExpr <@ fun x-> x * 5.0  @> 
+        
+    [<Test>] 
+    member x.``Extract common factors duplicated`` ()=
+        let e = simplify <@ fun (x:double) -> (x * 1.0 ) + (1.0 * x ) @>
+        e |> should equalExpr <@ fun x-> x * 2.0  @> 
+        
+    [<Test>] 
+    member x.``Extract common factors duplicated again`` ()=
+        let e = simplify <@ fun (x:double) -> (1.0 * x ) + (x * 1.0) @>
+        e |> should equalExpr <@ fun x-> 2.0 * x  @> 
--- Shambolics/Symbolics.fs
+++ Shambolics/Symbolics.fs
@@ -10,61 +10,117 @@ open Microsoft.FSharp.Quotations
 open FSharpx.Linq.QuotationEvaluation
 open Microsoft.FSharp.Linq.RuntimeHelpers
 
-module Calculus =
 
+module Calculus =
+        
     let rec simplify quotation =
     
+        let (|Same|_|) x =
+            match x with
+            | (r,l) when r = l -> Some(Same(l))
+            | _ -> None
+            
+        let (|Multiply|_|) x =
+            match x with    
+            | SpecificCall <@ (*) @> (_,_,l::r::[]) -> Some(Multiply(l,r))
+            | _ -> None 
+            
+        let (|Add|_|) x =
+            match x with    
+            | SpecificCall <@ (+) @> (_,_,l::r::[]) -> Some(Add(l,r))
+            | _ -> None 
+            
+        let (|Divide|_|) x =
+            match x with    
+            | SpecificCall <@ (+) @> (_,_,l::r::[]) -> Some(Divide(l,r))
+            | _ -> None 
+            
+        let (|ConstantMultiply|_|) x =
+            match x with
+            | Multiply( Double v, r) -> Some(ConstantMultiply(v,r))
+            | Multiply( l, Double v) -> Some(ConstantMultiply(v,l))    
+            | _ -> None
+            
+        let (|MultiplyAdd|_|) x =
+            match x with    
+            | Add ( Multiply (ll,lr), Multiply(rl, rr)) -> Some( MultiplyAdd( (ll,lr,rl,rr)))
+            | _ -> None    
+            
+        let Multiply3(a,b,c) = 
+            let e = simplify <@@(%%b:double) + (%%c:double) @@>
+            simplify <@@ (%%a: double) * (%%e:double) @@>
+        
+        let (|CommonFactor|_|) x =
+            match x with
+            | MultiplyAdd(al,ar,bl,br) when al = bl ->
+                Some(CommonFactor(Multiply3(al,ar,br)))
+                
+            | MultiplyAdd (al,ar,bl,br) when al = br ->
+                Some(CommonFactor(Multiply3(al,ar,bl)))
+                
+            | MultiplyAdd(al,ar,bl,br) when ar = bl ->
+                Some(CommonFactor(Multiply3(ar,al,br)))
+                
+            | MultiplyAdd(al,ar,bl,br) when ar = br ->
+                Some(CommonFactor(Multiply3(ar,al,bl)))
+                
+            | _ -> None
+    
+        
+        Console.WriteLine(quotation)
+        
         match quotation with
         
-        | SpecificCall <@ (+) @> (_,types,l::r::[]) -> 
+        | CommonFactor e -> e
+        
+        | Add (l,r) ->
         
             let ll = simplify l
             let rr = simplify r
             
-            
             match (ll,rr) with
-            | (Double 0.0, Double 0.0) -> <@@ 0.0 @@>
+            | (Double 0.0, Double 0.0) -> Expr.Value 0.0
             | (Double 0.0, v) -> v
             | (v, Double 0.0) -> v
-            | (Double a, Double b) -> 
-                Expr.Value (a + b)
+            | (Double a, Double b) -> Expr.Value (a + b)
+            | Same l -> <@@ 2.0 * (%%l:double) @@>
             | _ -> <@@ (%%ll:double) + (%%rr:double) @@>
                 
                 
-        | SpecificCall <@ (/) @> (_,types,l::r::[]) -> 
+        | Divide (l,r) ->
         
             let ll = simplify l
             let rr = simplify r
             
             match (ll,rr) with
-            | (_, Double 0.0) -> failwith "divide by zero"
-            | (Double 0.0, _) -> <@@ 0.0 @@>
-            | (Double a, Double b) -> 
-                Expr.Value (a / b)
-            | (v, Double 1.0) -> v 
-            | _ -> <@@ (%%ll:double) / (%%rr:double) @@>
+            | (_, Double 0.0)      -> failwith "divide by zero"
+            | (Double 0.0, _)      -> Expr.Value 0.0
+            | (Double a, Double b) -> Expr.Value (a / b)
+            | (v, Double 1.0)      -> v
+            | _                    -> <@@ (%%ll:double) / (%%rr:double) @@>
             
-                
-        | SpecificCall <@ (*) @> (_,types,l::r::[]) -> 
-        
-            let ll = simplify l
-            let rr = simplify r
+        | ConstantMultiply( c0, ConstantMultiply(c1, v)) ->    
+            let c = Expr.Value ( c0 * c1 )
+            simplify <@@ (%%c:double) * (%%v:double) @@>    
             
-            match (ll,rr) with
-            | (_, Double 0.0) -> <@@ 0.0 @@>
-            | (Double 0.0, _) -> <@@ 0.0 @@>
-            | (Double 1.0, v) -> v 
-            | (v, Double 1.0) -> v 
-            | (Double a, Double b) -> 
-                Expr.Value (a * b)
-            | _ -> <@@ (%%ll:double) * (%%rr:double) @@>
+        | ConstantMultiply( 0.0, v)      -> Expr.Value 0.0
+        | ConstantMultiply( 1.0, v)      -> v
+        | ConstantMultiply( c, Double v) -> Expr.Value (c * v )
+        | Multiply (a,b)                 -> 
+            let sa = simplify a
+            let sb = simplify b
+            Console.WriteLine("--")
+            Console.WriteLine(sa)
+            Console.WriteLine(sb)
+            let e = <@@ (%%sa:double) * (%%sb:double) @@>
+            if sa = a && sb = b then
+                e
+            else
+                simplify e
             
             
-        | ExprShape.ShapeVar v ->
-            Expr.Var v
-            
-        | ExprShape.ShapeLambda (v,expr) -> Expr.Lambda (v, simplify expr)
-        
+        | ExprShape.ShapeVar v                  -> Expr.Var v
+        | ExprShape.ShapeLambda (v,expr)        -> Expr.Lambda (v, simplify expr)
         | ExprShape.ShapeCombination (o, exprs) -> ExprShape.RebuildShapeCombination (o,List.map simplify exprs)
     
     let rec der_impl param quotation =
@@ -81,14 +137,17 @@ module Calculus =
         | SpecificCall <@ Math.Sin @> (_,_types, arg::_) ->
             let di = der_impl param arg
             <@@ (%%di:double) * Math.Cos( (%%arg:double) ) @@> 
+
+        | SpecificCall <@ Math.Cos @> (_,_types, arg::_) ->
+            let di = der_impl param arg
+            <@@ - (%%di:double) * Math.Sin( (%%arg:double) ) @@> 
             
-         | Double _ ->
-            <@@ 0.0 @@>
+         | Double _ -> Expr.Value 0.0
             
         | ExprShape.ShapeVar v -> 
             match v with 
-            | X -> <@@ 1.0 @@>
-            | _ -> (Expr.Var v)
+            | X -> Expr.Value 1.0
+            | _ -> Expr.Var v
             
         | ExprShape.ShapeLambda (v,expr) -> Expr.Lambda (v,der_impl param expr)
         
@@ -97,6 +156,5 @@ module Calculus =
     let rec der expr =
         match expr with
         | Lambda(param, body) ->
-            Expr.Lambda(param, (der_impl param body))
-            |> simplify
+            Expr.Lambda(param, (der_impl param body) |> simplify)
         | _ -> failwith "oops"
